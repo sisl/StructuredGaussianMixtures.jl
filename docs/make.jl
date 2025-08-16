7c125689aed538e07602e2f0f1ef577cb0f6109e
Arec Jamgochian <jamgochian95@gmail.com>
2025-08-16 13:54:41 -0700

    setting up deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -19,9 +19,7 @@ makedocs(
     remotes = nothing
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/sisl/StructuredGaussianMixtures.jl",
+    push_preview = true
+)
